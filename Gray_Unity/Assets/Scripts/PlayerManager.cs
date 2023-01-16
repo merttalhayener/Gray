3aec02b489bf6cbcec2d6d078739fb054a517a75
merttalhayener <mertshp@hotmail.com>
2023-01-16 09:10:33 +0300

    HerşeyHazırGibi

--- Gray_Unity/Assets/Scripts/PlayerManager.cs
+++ Gray_Unity/Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,22 @@ public class PlayerManager : MonoBehaviour
    
     public float playerHealth=100f;
     public bool playerIsAlive;
+    public Animator playerAnimator;
+    public ThirdPersonController tpController;
+    
    
     void Update()
     {
         Debug.Log(playerHealth);
         PlayerHealthManager();
+       
     }
     private void Awake()
     {
         playerIsAlive = true;
-        playerHealth = 10000f;
+        playerHealth = 100f;
+        playerAnimator.SetBool("PlayerIsAlive", playerIsAlive);
+
     }
 
     void PlayerHealthManager()
@@ -26,11 +33,15 @@ public class PlayerManager : MonoBehaviour
         {
             playerHealth = 0;
             playerIsAlive = false;
+            tpController.enabled = false;
+            playerAnimator.SetBool("PlayerIsAlive", playerIsAlive);
+            
         }
 
         if(playerHealth > 0)
         {
             playerIsAlive = true;
+            playerAnimator.SetBool("PlayerIsAlive", playerIsAlive);
         }
     }
 
